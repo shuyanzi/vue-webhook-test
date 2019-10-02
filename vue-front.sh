64765338c2374c2aed67f092dbaa18316372b3cd
jin.fang <jin.fang@advance.ai>
2019-10-02 20:21:40 +0800

    update

---
+++ vue-front.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+WORK_PATH = '/usr/projects/vue-front-test'
+cd $WORK_PATH
+echo 'clean old code'
+git reset --hard origin/master
+git clean -f
+echo 'pull lastest code'
+git pull origin master
+npm run build
+echo 'start build'
+docker build -t vue-front:1.0 .
+echo 'stop and remove old container'
+docker stop vue-front-container
+docker rm vue-front-container
+echo 'start new  container'
+docker container run -p 8000:8000 --name vue-front-container -d vue-front:1.0
